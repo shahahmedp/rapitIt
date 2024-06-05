eeee0541d345b54c3428c69f46fda753e328eaae
shahahmedp <shahbaazcd@gmail.com>
2024-06-05 17:26:20 +0530

    packages

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,22 +1,27 @@
 import { dbConfig } from '../config/config';
-import { 
-  initPostgres,
-} from './postgreSQL';
+import { initPostgres } from './postgreSQL';
 import { initMongo } from './mongoDB/index';
 import { dbConstants } from '../constants/repoConstants';
 //import mongoose from 'mongoose';
 import { logger } from '../Logger/index';
 
 const dbInit = async () => {
-  try{
-    dbConfig.type === dbConstants.MONGO ? await initMongo()
-      :dbConfig.type === dbConstants.POSTGRES ? await initPostgres()
-      :dbConfig.type === dbConstants.BOTH ? async () => { await initPostgres(); await initMongo()}
-      : () => { throw new Error('Unsupported database type')}
-  }catch(err){
-    logger.error({ message:  err });
+  try {
+    dbConfig.type === dbConstants.MONGO
+      ? await initMongo()
+      : dbConfig.type === dbConstants.POSTGRES
+        ? await initPostgres()
+        : dbConfig.type === dbConstants.BOTH
+          ? async () => {
+              await initPostgres();
+              await initMongo();
+            }
+          : () => {
+              throw new Error('Unsupported database type');
+            };
+  } catch (err) {
+    logger.error({ message: err });
   }
-  
 };
 
 export { dbInit };
